de25726e431df597b920ab209255d1ae8c7decab
Chibuzor Ifeanyi Ojukwu <chibuzor.ojukwu@gmail.com>
2023-08-05 23:38:01 +0100

    Update main.go

--- main.go
+++ main.go
@@ -127,7 +127,7 @@ func main() {
 		WriteTimeout: 5 * time.Second,
 	}
 
-	panic("OKOK")
+	panic("Could not connect to the database" + os.GetPid())
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		type PageVariables struct {
